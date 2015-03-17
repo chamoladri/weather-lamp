7b0df056d9170c38b5688e3c8fb1991224595ad0
chamoladri <mauropetrelli@gmail.com>
2015-03-16 23:29:42 -0300

    class

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 
 import pywapi
 import pprint
-
 pp = pprint.PrettyPrinter(indent=4)
 
-buneosAires = pywapi.get_weather_from_weather_com('ARBA0009:1:AR')
+
 
 #pp.pprint(buneosAires)
 #print buneosAires
 
-print buneosAires['current_conditions']['temperature']
-print buneosAires['current_conditions']['temperature']
-print buneosAires['current_conditions']['temperature']
-print buneosAires['current_conditions']['temperature']
+#print buneosAires['current_conditions']['temperature']
+
+
+class webTempweather_com:
+    
+    def __init__(self,locationId):
+        self.locationId = locationId
+      
+    def getTemp(self):
+        self.buenos = pywapi.get_weather_from_weather_com(self.locationId)
+        return self.buenos['current_conditions']['temperature']
+         
+    def getAllinfo(self):
+        self.a = pp.pprint(self.buenos)
+        return self.a     
+
+
+if __name__ == '__main__':     
+    
+    #incializando la clase 
+    temp=webTempweather_com('ARBA0009:1:AR')
+    print temp.getTemp()
+    print temp.getAllinfo()
